58201d39e09d05adf940308b8ed897c8e9fb115f
Maksim Kropanov <kropanov@gmail.com>
2024-11-27 11:49:47 +0300

    feat: connect to the camera

--- src/arduinobot_description/launch/gazebo.launch.py
+++ src/arduinobot_description/launch/gazebo.launch.py
@@ -2,15 +2,18 @@ import os
 from pathlib import Path
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.actions import (DeclareLaunchArgument, SetEnvironmentVariable, 
+                            IncludeLaunchDescription, SetLaunchConfiguration)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, TextSubstitution, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
     arduinobot_description_dir = get_package_share_directory("arduinobot_description")
+    gz_model_path = PathJoinSubstitution([arduinobot_description_dir])
+    gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
 
     model_arg = DeclareLaunchArgument(
         name="model", 
@@ -56,6 +59,30 @@ def generate_launch_description():
         arguments=["-topic", "robot_description", "-name", "arduinobot"]
     )
 
+    bridge_params = os.path.join(
+        get_package_share_directory('arduinobot_description'),
+        'params',
+        'arduinobot_bridge.yaml'
+    )
+
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+
+    start_gazebo_ros_image_bridge_cmd = Node(
+        package='ros_gz_image',
+        executable='image_bridge',
+        arguments=['/rgb_camera/image_raw'],
+        output='screen',
+    )
+
     gz_ros2_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
@@ -65,10 +92,29 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='my_world',
+            description='World to load into Gazebo'
+        ),
+        SetLaunchConfiguration(name='world_file', 
+                               value=[LaunchConfiguration('world'), 
+                                      TextSubstitution(text='.sdf')]),
+        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': [PathJoinSubstitution([arduinobot_description_dir, 'worlds',
+                                                  LaunchConfiguration('world_file')])],
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
         model_arg,
         gazebo_resource_path,
         robot_state_publisher,
-        gazebo,
         gz_spawn_entity,
-        gz_ros2_bridge
+        gazebo,
+        gz_ros2_bridge,
+        start_gazebo_ros_bridge_cmd,
+        start_gazebo_ros_image_bridge_cmd
     ])
